9524c619744acb4e75eae2d0839daedfd1ca7ae4
Sebastian Börjesson <sebastianborjesson@Sebastians-MBP.lan>
2015-01-24 20:01:08 +0100

    written som queries for saving pages

--- php/classes/contentqueries.class.php
+++ php/classes/contentqueries.class.php
@@ -2,19 +2,55 @@
 
 class ContentQueries extends PDOHelper {
 
+  //later when we have login in place, real user_info 
+  //will be stored in the property user_info.
+  //for now let's just fake it
+  protected $user_info = array("user_id" => 1);
 
-	public function saveNewPage ($page_data) {
-		/*
-			$page_data = array (
-				":title" => "string",
-				":body" => "string",
-				":user_id"=> int",
+	public function storeNewPage ($page_data) {
+		
+		//adding user_id before insert
+    	$page_data[":user_id"] = $this->user_info["user_id"];
 
-			)
-		*/
 		$sql = "INSERT INTO pages (title, body, user_id) VALUES (:title, :body, :user_id)";
 		return $this->query($sql, $page_data);
 	}
+
+	public function saveNewUrl ($url_data) {
+		
+		$sql = "SELECT pid FROM pages ORDER BY created DESC LIMIT 1";
+    	$new_pid = $this->query($sql);
+
+    	$new_pid = $new_pid[0]["pid"];
+
+	    $url_path = $url_data[":path"];
+
+	    $sql2 = "INSERT INTO url_alias (path, pid) VALUES (:path, :pid)";
+	    $url_info = array(":path" => $page_path, ":pid" => $new_pid);
+	    
+	    return $this->query($sql, $url_info);
+	}
+
+	public function addMenuLink ($menu_data) {
+		$sql = "INSERT INTO menu_links (title, path, menu, plid, weight) VALUES (:title, :path, :menu_name, :plid, :weight)";
+		$this->query($sql, $menu_data);
+
+	}
+
+	public function getMenuNames() {
+	    $sql = "SELECT * FROM menus";
+	    return $this->query($sql);
+	  }
+
+
+  public function getMenuLinks($menu_name) {
+	    $menu_name = array(":menu_name" => $menu_name);
+	    $sql = "SELECT * FROM menu_links WHERE menu = :menu_name";
+	    
+	    return $this->query($sql, $menu_name);
+	  }
+
+
 
 	public function getAllPages() {
 		$sql = "SELECT * FROM pages";
--- php/save_content.php
+++ php/save_content.php
@@ -1,3 +1,11 @@
 <?php
 
 include_once ("autoloader.php");
+
+$cg = New ContentQueries("127.0.0.1","cms_example","root","mysql");
+
+// save content if told to do so (by receiving correct AJAX data)
+if (isset($_REQUEST["page_data"])) {
+  //save page and echo ContentQueries response
+  echo(json_encode($cq->storeNewPage($_REQUEST["page_data"])));
+}
